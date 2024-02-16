a2acb8699b2b6316b3e4085ae94087b3acf47ea6
Sebastian Urwan <surwan@habana.ai>
2024-02-16 16:12:06 +0200

    Changed Habana copyright header

--- vllm/hpu/cache_ops.py
+++ vllm/hpu/cache_ops.py
@@ -1,13 +1,5 @@
 ###############################################################################
-# Copyright (C) 2023 Habana Labs, Ltd. an Intel Company
-# All Rights Reserved.
-#
-# Unauthorized copying of this file or any element(s) within it, via any medium
-# is strictly prohibited.
-# This file contains Habana Labs, Ltd. proprietary and confidential information
-# and is subject to the confidentiality and license agreements under which it
-# was provided.
-#
+# Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
 ###############################################################################
 
 from typing import Tuple
