702d8a733ea8ef65970ff3bb81bcbbe5aeae4aa4
Sebastian Urwan <surwan@habana.ai>
2024-02-16 16:27:17 +0200

    Added BSD license header

--- vllm/hpu/cuda_utils.py
+++ vllm/hpu/cuda_utils.py
@@ -1,5 +1,8 @@
 ###############################################################################
 # Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
 ###############################################################################
 
 def get_device_attribute(attribute, device_id):
--- vllm/hpu/ops.py
+++ vllm/hpu/ops.py
@@ -1,5 +1,8 @@
 ###############################################################################
 # Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
 ###############################################################################
 
 import torch
