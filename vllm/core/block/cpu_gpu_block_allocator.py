1f2c07e1d82297bb9e0a4a3b2742601e55a00bad
Michal Adamczyk <madamczyk@habana.ai>
2024-07-24 14:03:53 +0300

    Fix failing assertion in NaiveBlockAllocator

--- vllm/core/block/cpu_gpu_block_allocator.py
+++ vllm/core/block/cpu_gpu_block_allocator.py
@@ -54,13 +54,14 @@ class CpuGpuBlockAllocator(DeviceAwareBlockAllocator):
         """
         reserved_blocks = 1 # We use block 0 as a scratchpad for padding purposes
         block_ids = list(range(reserved_blocks, num_gpu_blocks + num_cpu_blocks))
+        num_gpu_blocks -= reserved_blocks
         gpu_block_ids = block_ids[:num_gpu_blocks]
         cpu_block_ids = block_ids[num_gpu_blocks:]
 
         if allocator_type == "naive":
             gpu_allocator: BlockAllocator = NaiveBlockAllocator(
                 create_block=NaiveBlock,  # type: ignore
-                num_blocks=num_gpu_blocks-reserved_blocks,
+                num_blocks=num_gpu_blocks,
                 block_size=block_size,
                 block_ids=gpu_block_ids,
             )
