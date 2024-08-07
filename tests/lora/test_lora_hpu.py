e154e3c1baa777026e2cd84513815854b058bc0c
JonghoLee <jongho.lee@squeezebits.com>
2024-08-07 08:56:12 +0000

    Implement cases that last N batches are non-LoRA

--- tests/lora/test_lora_hpu.py
+++ tests/lora/test_lora_hpu.py
@@ -19,14 +19,16 @@ TOLERANCES = {
     torch.float16: (5e-3, 5e-3),
     torch.bfloat16: (3e-2, 2e-2),
 }
-
+# Assume that last N batches are no LoRA case
+NO_LORA_BATCHES = [2]
 
 @pytest.mark.parametrize("m", TENSOR_SIZES)
 @pytest.mark.parametrize("n", TENSOR_SIZES)
 @pytest.mark.parametrize("k", BATCH_SIZES)
 @pytest.mark.parametrize("rank", RANKS)
 @pytest.mark.parametrize("dtype", DTYPES)
-def test_apply_lora(m, n, k, rank, dtype) -> None:
+@pytest.mark.parametrize("N", NO_LORA_BATCHES)
+def test_apply_lora(m, n, k, rank, dtype, N) -> None:
     manager = DummyLoRAManager()
 
     module_name = "module"
@@ -37,6 +39,7 @@ def test_apply_lora(m, n, k, rank, dtype) -> None:
 
     input = torch.rand(k, n, device="hpu", dtype=dtype)
     expected = input @ lora.lora_a @ lora.lora_b * lora.scaling
+    expected[-N:, :] = torch.zeros(N, expected.shape[1])
 
     lora_a_stack = torch.zeros(8,
                                1,
@@ -50,22 +53,22 @@ def test_apply_lora(m, n, k, rank, dtype) -> None:
                                lora.lora_b.shape[0],
                                device="hpu",
                                dtype=dtype)
-    for i in range(lora_a_stack.shape[0]):
+    for i in range(lora_a_stack.shape[0] - N):
         lora_a_stack[i][0] = lora.lora_a.T
         lora_b_stack[i][0] = (lora.lora_b * lora.scaling).T
 
     output = torch.zeros(k, m, device="hpu", dtype=dtype)
+    indices = torch.randint(0, lora_a_stack.shape[0]-N, (len(input), ), device="hpu")
+    indices[-N:] = -1
     _apply_lora(
-        input, lora_a_stack, lora_b_stack,
-        torch.randint(0, lora_a_stack.shape[0], (len(input), ), device="hpu"),
-        output)
-
+        input, lora_a_stack, lora_b_stack, indices, output)
     rtol, atol = TOLERANCES[dtype]
     assert torch.allclose(expected, output, rtol=rtol, atol=atol)
 
     output[:] = 0
-    _apply_lora(input, lora_a_stack, lora_b_stack,
-                torch.full((len(input), ), -1, device="hpu"), output)
+    indices = torch.full((len(input), ), -1, device="hpu")
+    _apply_lora(
+        input, lora_a_stack, lora_b_stack, indices, output)
     assert torch.allclose(torch.zeros_like(output), output)
 
     manager.reset_lora()
@@ -76,7 +79,8 @@ def test_apply_lora(m, n, k, rank, dtype) -> None:
 @pytest.mark.parametrize("k", BATCH_SIZES)
 @pytest.mark.parametrize("rank", RANKS)
 @pytest.mark.parametrize("dtype", DTYPES)
-def test_apply_lora_packed_2slice(m, n, k, rank, dtype) -> None:
+@pytest.mark.parametrize("N", NO_LORA_BATCHES)
+def test_apply_lora_packed_2slice(m, n, k, rank, dtype, N) -> None:
     if m % 2 != 0:
         pytest.skip("m must be divisible by 2")
     if m // 2 not in TENSOR_SIZES:
@@ -98,6 +102,7 @@ def test_apply_lora_packed_2slice(m, n, k, rank, dtype) -> None:
         input @ lora_2.lora_a @ lora_2.lora_b * lora_2.scaling
     ],
                          dim=1)
+    expected[-N:, :] = torch.zeros(N, expected.shape[1])
 
     lora_a_stacks = [
         torch.zeros(8,
@@ -115,26 +120,27 @@ def test_apply_lora_packed_2slice(m, n, k, rank, dtype) -> None:
                     device="hpu",
                     dtype=dtype) for i in range(2)
     ]
-    for i in range(lora_a_stacks[0].shape[0]):
+    for i in range(lora_a_stacks[0].shape[0] - N):
         lora_a_stacks[0][i][0] = lora_1.lora_a.T
         lora_b_stacks[0][i][0] = (lora_1.lora_b * lora_1.scaling).T
         lora_a_stacks[1][i][0] = lora_2.lora_a.T
         lora_b_stacks[1][i][0] = (lora_2.lora_b * lora_2.scaling).T
 
     output = torch.zeros(k, m, device="hpu", dtype=dtype)
+    indices = torch.randint(0, lora_a_stacks[0].shape[0]-N, (len(input), ), device="hpu")
+    indices[-N:] = -1
     _apply_lora_packed_nslice(
         input, lora_a_stacks, lora_b_stacks,
-        torch.randint(0,
-                      lora_a_stacks[0].shape[0], (len(input), ),
-                      device="hpu"), output, (m // 2, m // 2))
+        indices, output, (m // 2, m // 2))
 
     rtol, atol = TOLERANCES[dtype]
     assert torch.allclose(expected, output, rtol=rtol, atol=atol)
 
     output[:] = 0
-    _apply_lora_packed_nslice(input, lora_a_stacks, lora_b_stacks,
-                              torch.full((len(input), ), -1, device="hpu"),
-                              output, (m // 2, m // 2))
+    indices = torch.full((len(input), ), -1, device="hpu")
+    _apply_lora_packed_nslice(
+        input, lora_a_stacks, lora_b_stacks,
+        indices, output, (m // 2, m // 2))
     assert torch.allclose(torch.zeros_like(output), output)
 
     manager.reset_lora()
@@ -145,7 +151,8 @@ def test_apply_lora_packed_2slice(m, n, k, rank, dtype) -> None:
 @pytest.mark.parametrize("k", BATCH_SIZES)
 @pytest.mark.parametrize("rank", RANKS)
 @pytest.mark.parametrize("dtype", DTYPES)
-def test_apply_lora_packed_3slice(qkv, n, k, rank, dtype) -> None:
+@pytest.mark.parametrize("N", NO_LORA_BATCHES)
+def test_apply_lora_packed_3slice(qkv, n, k, rank, dtype, N) -> None:
     manager = DummyLoRAManager()
 
     module_name = "module"
@@ -160,12 +167,14 @@ def test_apply_lora_packed_3slice(qkv, n, k, rank, dtype) -> None:
     lora_v = manager.get_module_lora(module_name + "v")
 
     input = torch.rand(k, n, device="hpu", dtype=dtype)
+    N = 1 # Assume that last N batches are no LoRA case
     expected = torch.cat([
         input @ lora_q.lora_a @ lora_q.lora_b * lora_q.scaling,
         input @ lora_k.lora_a @ lora_k.lora_b * lora_k.scaling,
         input @ lora_v.lora_a @ lora_v.lora_b * lora_v.scaling
     ],
                          dim=1)
+    expected[-N:, :] = torch.zeros(N, expected.shape[1])
 
     lora_a_stacks = [
         torch.zeros(8,
@@ -197,7 +206,7 @@ def test_apply_lora_packed_3slice(qkv, n, k, rank, dtype) -> None:
                     device="hpu",
                     dtype=dtype) for i in range(2)
     ]
-    for i in range(lora_a_stacks[0].shape[0]):
+    for i in range(lora_a_stacks[0].shape[0] - N):
         lora_a_stacks[0][i][0] = lora_q.lora_a.T
         lora_b_stacks[0][i][0] = (lora_q.lora_b * lora_q.scaling).T
         lora_a_stacks[1][i][0] = lora_k.lora_a.T
@@ -206,20 +215,21 @@ def test_apply_lora_packed_3slice(qkv, n, k, rank, dtype) -> None:
         lora_b_stacks[2][i][0] = (lora_v.lora_b * lora_v.scaling).T
 
     output = torch.zeros(k, sum(qkv), device="hpu", dtype=dtype)
+    indices = torch.randint(0, lora_a_stacks[0].shape[0] - N, (len(input), ), device="hpu")
+    indices[-N:] = -1
     _apply_lora_packed_nslice(
         input, lora_a_stacks, lora_b_stacks,
-        torch.randint(0,
-                      lora_a_stacks[0].shape[0], (len(input), ),
-                      device="hpu"), output, (qkv[0], qkv[1], qkv[2]))
+        indices, output, (qkv[0], qkv[1], qkv[2]))
 
     rtol, atol = TOLERANCES[dtype]
     # import pdb; pdb.set_trace()
     assert torch.allclose(expected, output, rtol=rtol, atol=atol)
 
     output[:] = 0
-    _apply_lora_packed_nslice(input, lora_a_stacks, lora_b_stacks,
-                              torch.full((len(input), ), -1, device="hpu"),
-                              output, (qkv[0], qkv[1], qkv[2]))
+    indices = torch.full((len(input), ), -1, device="hpu")
+    _apply_lora_packed_nslice(
+        input, lora_a_stacks, lora_b_stacks,
+        indices, output, (qkv[0], qkv[1], qkv[2]))
     assert torch.allclose(torch.zeros_like(output), output)
 
     manager.reset_lora()
